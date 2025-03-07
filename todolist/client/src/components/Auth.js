76f52dee357579a6d1186faf34cb242904724660
XJQ0909 <random202420252026@outlook.com>
2025-03-07 14:42:06 -0500

    Update Auth.js

--- todolist/client/src/components/Auth.js
+++ todolist/client/src/components/Auth.js
@@ -24,12 +24,6 @@ const Auth =() => {
       return
     }
 
-    // const response = await fetch(`${process.env.REACT_APP_SERVERURL}/${endpoint}`,{
-    //   method: 'POST',
-    //   headers: {'Content-Type':'application/json'},
-    //   body: JSON.stringify({username,password})
-    // })
-
     axios.defaults.withCredentials = true;
     const response = await axios.post(
       `${process.env.REACT_APP_SERVERURL}/${endpoint}`,
